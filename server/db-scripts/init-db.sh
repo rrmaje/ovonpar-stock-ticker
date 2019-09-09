cd0640b070ed7c7e0a5aa4cd683b972df985ed43
rrmaje <rafal.majewski@gmail.com>
2019-09-09 21:14:48 +0200

    Fix db-init scripts

--- server/db-scripts/init-db.sh
+++ server/db-scripts/init-db.sh
@@ -1,5 +1,5 @@
-docker cp postgres:/tmp default/1.sql 
-docker cp postgres:/tmp default/2.sql  
+docker cp default/1.sql postgres:/tmp 
+docker cp default/2.sql postgres:/tmp 
 
 docker exec -it postgres psql -U postgres -f /tmp/1.sql
 docker exec -it postgres psql -U postgres -f /tmp/2.sql
