9b473d990132e8ca892d184f02e8ec0303a5881a
rrmaje <rafal_majewski@instance-1.europe-west4-a.c.ovonpar-system.internal>
2019-09-10 12:12:18 +0000

    Fix startup scripts

--- server/build.sh
+++ server/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker run --rm --network="tickernet" --name ticker -p9000:9000 -p9443:9443 -e OST_MULTICAST_INTERFACE=eth0 \
--e OST_REQUEST_ADDRESS=parity-system rrmaje/parity-ticker:0.1.0-SNAPSHOT
+docker run --rm -d --network="tickernet" --name ticker -p9000:9000 -p9443:9443 --env-file .env.prod rrmaje/parity-ticker:0.1.0-SNAPSHOT
--- server/db-scripts/init-db.sh
+++ server/db-scripts/init-db.sh
@@ -1,6 +1,5 @@
 docker cp default/1.sql postgres:/tmp 
 docker cp default/2.sql postgres:/tmp 
 
-docker exec -it postgres psql -U postgres -f /tmp/1.sql
-docker exec -it postgres psql -U postgres -f /tmp/2.sql
+docker exec -it postgres psql -U postgres -f /tmp/1.sql && docker exec -it postgres psql -U postgres -f /tmp/2.sql
 
